d0726051117c76d93e02566bf555cb5f614f42d6
Shivek Khurana <khuranashivek@gmail.com>
2014-02-08 20:50:39 +0530

    making better data analysis system

--- load_log.m
+++ load_log.m
@@ -6,7 +6,6 @@ log_file = input('Enter file to visualize : ', 's');
 
 data = load(log_file);
 
-
 global t  = data(:,1);
 global ax = data(:,2);
 global ay = data(:,3);
@@ -14,6 +13,6 @@ global az = data(:,4);
 global wx = data(:,5);
 global wy = data(:,6);
 global wz = data(:,7);
-
-
+global loaded_pattern = log_file;
 printf('Loaded file %s\nThe following variables are available : t, ax, ay, az, wx, wy, wz\n', log_file);
+clear log_file, data;
---
+++ load_validator.m
@@ -0,0 +1,9 @@
+required_variables = {'t'; 'ax'; 'ay'; 'az'; 'wx'; 'wy'; 'wz'};
+for i=1:length(required_variables)
+	if(exist(required_variables{i}, 'var') == 0)
+		printf('%s not loaded [%d]\n', required_variables{i}, exist(required_variables{i}, 'var'));
+		break;
+	else
+		printf('%s loaded\n', required_variables{i});
+	end
+end
