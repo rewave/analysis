2a3b2b4508d2f3a99fbe057fafc0a956a2bd1b4a
Shivek Khurana <khuranashivek@gmail.com>
2014-05-14 13:53:31 +0530

    Still need a way to visualize motion

--- transformations.m
+++ transformations.m
@@ -1,8 +1,8 @@
-load_log('o.txt', true);
+load_log('a.txt', true);
 
 global t ax ay az wx wy wz;
 
-function [x,y,z] = true_coordinates(ax,ay,az,gx,gy, gz)
+function [x,y,z] = true_coordinates(ax,ay,az,gx,gy,gz)
 	x = (ax*(cos(gz)*cos(gx)+sin(gz)*sin(gy)*sin(gx)) + ay*(cos(gy)*sin(gx)) + az*(-sin(gz)*cos(gx)+cos(gz)*sin(gy)*sin(gx)));
 	y = (ax*(-cos(gz)*sin(gx)+sin(gz)*sin(gy)*cos(gx)) + ay*(cos(gy)*cos(gx)) + az*(sin(gz)*sin(gx)+ cos(gz)*sin(gy)*cos(gx)));;
 	z = (ax*(sin(gz)*cos(gy)) + ay*(-sin(gy)) + az*(cos(gz)*cos(gy)));
@@ -11,7 +11,12 @@ end
 p = [];
 
 for i=1:length(t)
-	p = [p; true_coordinates(ax(i), ay(i), az(i), wx(i), wy(i), wz(i))];
+	[x y z] = true_coordinates(ax(i), ay(i), az(i), wx(i), wy(i), wz(i));
+	p = [p; [x y z]];
 end
 
-plot3(p)
+ax = p(:,1);
+ay = p(:, 2);
+az = p(:, 3);
+
+plot_displacement();
